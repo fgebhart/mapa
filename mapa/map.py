9e334f0e956e8e91756f7e27dc2c36ffb4ba56bf
Fabian Gebhart <fabian.gebhart@gmail.com>
2022-04-18 20:16:08 +0200

    work in progress: adding a func for drawing a rect on the map

--- mapa/map.py
+++ mapa/map.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple
 
-from ipyleaflet import DrawControl, Map, ScaleControl, basemap_to_tiles, basemaps
+from ipyleaflet import DrawControl, Map, Rectangle, ScaleControl, basemap_to_tiles, basemaps
 from ipywidgets import Layout
 
 CENTER = [40.5566, 23.4660]
@@ -20,3 +20,11 @@ def show_map(center: List[float] = CENTER, zoom: int = ZOOM) -> Tuple[Map, DrawC
     dc.on_draw(handle_draw)
     m.add_control(dc)
     return m, dc
+
+
+def draw_rect_on_map(rect_coordinates: List[List[float]]) -> Map:
+    coords = [i[::-1] for i in rect_coordinates]  # swap position because of GeoJSON
+    m = Map(center=coords[0], zoom=5, scroll_wheel_zoom=True)
+    rectangle = Rectangle(bounds=coords)
+    m.add_layer(rectangle)
+    return m
